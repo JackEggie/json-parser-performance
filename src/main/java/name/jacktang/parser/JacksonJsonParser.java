ff75450d27fad6b0474ec42c10c2a7b198334043
tangzhaowei <tangzhaowei@hdfax.com>
2018-03-07 11:50:11 +0800

    init json performance

---
+++ src/main/java/name/jacktang/parser/JacksonJsonParser.java
@@ -0,0 +1,34 @@
+package name.jacktang.parser;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * @author tangzhaowei
+ */
+public class JacksonJsonParser implements JsonParser {
+
+    ObjectMapper objectMapper;
+
+    public void init() {
+        objectMapper = new ObjectMapper();
+    }
+
+    public String serialize(Object o) {
+        try {
+            return objectMapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object parse(String s) {
+        try {
+            return objectMapper.readValue(s, Object.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
